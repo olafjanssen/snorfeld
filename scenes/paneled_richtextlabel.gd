25d672227e9e7dcdd98f00473c01f3685c998308
O.T.A. Janssen <o.t.a.janssen@gmail.com>
2026-05-02 11:39:55 +0200

    Fix style and suggestion labels

--- scenes/paneled_richtextlabel.gd
+++ scenes/paneled_richtextlabel.gd
@@ -5,5 +5,4 @@ extends PanelContainer
 @onready var TextField: ClickableRichTextLabel = $MarginContainer/ClickableRichTextLabel
 
 func set_text(full_text : String):
-	pass 
 	TextField.text = full_text
