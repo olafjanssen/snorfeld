6734c89b795502cd3a1c38c86daab59d0a9cdaeb
O.T.A. Janssen <o.t.a.janssen@gmail.com>
2026-05-02 16:38:15 +0200

    Removing the git menu

--- scenes/editor.gd
+++ scenes/editor.gd
@@ -3,5 +3,4 @@ extends Control
 func _ready():
 	await get_tree().process_frame
 	$VBoxContainer.offset_top = $MenuBar.size.y
-
 	$VBoxContainer/HSplitContainer.split_offsets = PackedInt32Array([200, 800, 1600])
